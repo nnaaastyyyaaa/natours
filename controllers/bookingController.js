1eb70e4af258dd7d580ad797daa024765e94a8fb
Anastasia Andrienko <nnaaastyyyaaa06@gmail.com>
2025-08-07 21:57:40 +0300

    Fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -11,7 +11,7 @@ exports.getCheckoutSession = async (req, res, next) => {
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
     mode: 'payment',
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/myTours`,
     // success_url: `${req.protocol}://${req.get('host')}/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`,
